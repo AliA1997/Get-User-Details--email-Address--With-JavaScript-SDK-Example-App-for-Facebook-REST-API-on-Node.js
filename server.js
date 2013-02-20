db1a7c0797a273f95fb3f8aceff505978f615a9c
Runnable <git@runnable.com>
2013-02-20 01:44:38 +0000

    changes from web session

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ var express = require('express')
   , app = express();
 
 app.use(express.static(__dirname));
-app.use(app.router);
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
